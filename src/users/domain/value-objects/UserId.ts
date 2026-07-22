3111a15f67fe9185cdfea2ea58eb82bab1eef33a
OlamidotunIY <olamidotun225@gmail.com>
2026-07-22 20:32:57 +0100

    feat: create Id class for user and wallet

---
+++ src/users/domain/value-objects/UserId.ts
@@ -0,0 +1,17 @@
+import { EntityId } from '@shared';
+
+class UserId extends EntityId {
+  constructor(value: string) {
+    super(value);
+  }
+
+  static create(): UserId {
+    return new UserId(crypto.randomUUID());
+  }
+
+  static fromString(value: string): UserId {
+    return new UserId(value);
+  }
+}
+
+export { UserId };
