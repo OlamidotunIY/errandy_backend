4def2d00a78fd564fa8e557a1813145bedb4f652
OlamidotunIY <olamidotun225@gmail.com>
2026-07-22 20:35:30 +0100

    add: add the execute method to fund escrow handler class

--- src/escrow/application/commands/FundEscrow/FundEscrowHandler.ts
+++ src/escrow/application/commands/FundEscrow/FundEscrowHandler.ts
@@ -1,12 +1,23 @@
-import { IEscrowRepository, IPlatformFeePolicy } from '@escrow';
+import {
+  FundEscrowCommand,
+  IEscrowRepository,
+  IPlatformFeePolicy,
+} from '@escrow';
 import { ILogger } from '@shared';
+import { IWalletRepository } from '@wallet';
+import { FundEscrowResult } from './FundEscrowResult';
 
 class FundEscrowHandler {
   constructor(
     private readonly escrowRepository: IEscrowRepository,
+    private readonly walletRepository: IWalletRepository,
     private readonly feePolicy: IPlatformFeePolicy,
     private readonly logger: ILogger,
   ) {}
+
+  async execute(command: FundEscrowCommand): Promise<FundEscrowResult> {
+    return await Promise.resolve({} as FundEscrowResult);
+  }
 }
 
 export { FundEscrowHandler };
