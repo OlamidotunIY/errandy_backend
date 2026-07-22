e49e9bef9b3f3662f6c35f7b05cbb05b3be7b9b2
OlamidotunIY <olamidotun225@gmail.com>
2026-07-22 18:47:04 +0100

    feat: create repository interface for escrow

--- src/escrow/domain/repository/IEscrowRepository.ts
+++ src/escrow/domain/repository/IEscrowRepository.ts
@@ -1,7 +1,18 @@
-import { Escrow, EscrowId } from '..';
+import { ErrandId } from '@errands';
+import { Escrow, EscrowId, EscrowStatus } from '..';
+import { ClientId } from '@client';
+import { ProviderId } from '@provider';
 
 interface IEscrowRepository {
   findById(id: EscrowId): Promise<Escrow | null>;
+  findByErrandId(errandId: ErrandId): Promise<Escrow | null>;
+  findByClientId(clientId: ClientId, status?: EscrowStatus): Promise<Escrow[]>;
+  findByWorkerId(
+    workerId: ProviderId,
+    status?: EscrowStatus,
+  ): Promise<Escrow[]>;
+  findMaturedForRelease(): Promise<Escrow[]>;
+  save(escrow: Escrow): Promise<void>;
 }
 
 export { IEscrowRepository };
