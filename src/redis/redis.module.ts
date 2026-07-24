f296eb22b0d14047a45601db8961be22b1c99da3
OlamidotunIY <olamidotun225@gmail.com>
2026-07-25 00:40:56 +0100

    update: add required class to wallet module

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -13,24 +13,13 @@ export const REDIS = Symbol('REDIS');
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
         const url = config.get<string>('REDIS_URL'); // e.g. redis://localhost:6379
-        const host = config.get<string>('REDIS_HOST') ?? 'redis';
-        const port = Number(config.get<string>('REDIS_PORT') ?? 6379);
-        const password = config.get<string>('REDIS_PASSWORD');
+        console.log(url);
 
-        const redis = url
-          ? new Redis(url, {
-              maxRetriesPerRequest: null,
-              enableReadyCheck: true,
-              lazyConnect: false,
-            })
-          : new Redis({
-              host,
-              port,
-              password,
-              maxRetriesPerRequest: null,
-              enableReadyCheck: true,
-              lazyConnect: false,
-            });
+        const redis = new Redis(url as string, {
+          maxRetriesPerRequest: null,
+          enableReadyCheck: true,
+          lazyConnect: false,
+        });
 
         redis.on('connect', () => console.log('[Redis] connected'));
         redis.on('error', (err) => console.error('[Redis] error', err));
