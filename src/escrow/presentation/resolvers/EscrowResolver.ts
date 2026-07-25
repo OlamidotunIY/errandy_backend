80be02587e404df041697e64eb4b4e2555b84bb6
OlamidotunIY <olamidotun225@gmail.com>
2026-07-25 20:01:29 +0100

    feat: implemented escrow resolver and graphql types

---
+++ src/escrow/presentation/resolvers/EscrowResolver.ts
@@ -0,0 +1,24 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { QueryBus } from '@nestjs/cqrs';
+import { EscrowGraphQLType } from '../graphql';
+import { CurrentUser } from '../../../auth/decorator/current-user.decorator';
+import { User } from '@user/entities/user.entity';
+import { GetEscrowByErrandQuery } from '@escrow/application';
+import { ErrandId } from '@errands';
+
+@Resolver()
+export class EscrowResolver {
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @Query(() => EscrowGraphQLType)
+  async escrowById(
+    @CurrentUser() user: User,
+    @Args('errandId') id: string,
+  ): Promise<EscrowGraphQLType> {
+    return (await this.queryBus.execute(
+      new GetEscrowByErrandQuery({
+        errandId: ErrandId.fromString(id),
+      }),
+    )) as EscrowGraphQLType;
+  }
+}
