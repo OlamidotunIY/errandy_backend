1874582bad3d51ca340df0e0ee58a963fc720b2e
OlamidotunIY <olamidotun225@gmail.com>
2026-07-25 10:50:38 +0100

    update: make class abstract and method also

--- src/escrow/domain/repository/IEscrowRepository.ts
+++ src/escrow/domain/repository/IEscrowRepository.ts
@@ -3,16 +3,19 @@ import { Escrow, EscrowId, EscrowStatus } from '..';
 import { ClientId } from '@client';
 import { ProviderId } from '@provider';
 
-interface IEscrowRepository {
-  findById(id: EscrowId): Promise<Escrow | null>;
-  findByErrandId(errandId: ErrandId): Promise<Escrow | null>;
-  findByClientId(clientId: ClientId, status?: EscrowStatus): Promise<Escrow[]>;
-  findByWorkerId(
+abstract class EscrowRepository {
+  abstract findById(id: EscrowId): Promise<Escrow | null>;
+  abstract findByErrandId(errandId: ErrandId): Promise<Escrow | null>;
+  abstract findByClientId(
+    clientId: ClientId,
+    status?: EscrowStatus,
+  ): Promise<Escrow[]>;
+  abstract findByWorkerId(
     workerId: ProviderId,
     status?: EscrowStatus,
   ): Promise<Escrow[]>;
-  findMaturedForRelease(): Promise<Escrow[]>;
-  save(escrow: Escrow): Promise<void>;
+  abstract findMaturedForRelease(): Promise<Escrow[]>;
+  abstract save(escrow: Escrow): Promise<void>;
 }
 
-export { IEscrowRepository };
+export { EscrowRepository };
