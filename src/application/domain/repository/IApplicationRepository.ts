486db65f756610ea8d50b89af01d8e4b1a7484aa
OlamidotunIY <olamidotun225@gmail.com>
2026-07-28 05:56:32 +0100

    add: application repository abstract class and abstract methods like save, fineById etc

---
+++ src/application/domain/repository/IApplicationRepository.ts
@@ -0,0 +1,15 @@
+import { ErrandId } from '@errands';
+import { Application } from '../entities';
+import { ApplicationId } from '../value-objects';
+import { ProviderId } from '@provider';
+
+export abstract class IApplicationRepository {
+  abstract save(application: Application): Promise<void>;
+  abstract findById(id: ApplicationId): Promise<Application | null>;
+  abstract findByErrandId(errandId: ErrandId): Promise<Application[]>;
+  abstract findByWorkerId(workerId: ProviderId): Promise<Application[]>;
+  abstract existsByErrandAndWorker(
+    errandId: ErrandId,
+    workerId: ProviderId,
+  ): Promise<boolean>;
+}
