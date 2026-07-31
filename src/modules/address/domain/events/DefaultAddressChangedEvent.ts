57cc779b89ecc9fff1f64d873def83ab3dc9250e
OlamidotunIY <olamidotun225@gmail.com>
2026-07-31 16:23:51 +0100

    feat(address): introduced default address changed event class

---
+++ src/modules/address/domain/events/DefaultAddressChangedEvent.ts
@@ -0,0 +1,37 @@
+import { DomainEvent } from '@src/common';
+import { Address, AddressId } from '@module/address';
+
+export class DefaultAddressChangedEvent implements DomainEvent {
+  readonly eventId: string;
+  readonly aggregateId: AddressId;
+  readonly eventName: string;
+  readonly correlationId: string;
+  readonly occurredAt: Date;
+
+  constructor(
+    aggregateId: AddressId,
+    occurredAt: Date,
+    correlationId: string,
+    public readonly payload: Record<string, unknown>,
+  ) {
+    this.eventName = 'DefaultAddressChangedEvent';
+    this.occurredAt = occurredAt;
+    this.eventId = crypto.randomUUID();
+    this.aggregateId = aggregateId;
+    this.correlationId = correlationId;
+  }
+
+  static fromAggregate(
+    address: Address,
+    correlationId: string,
+  ): DefaultAddressChangedEvent {
+    return new DefaultAddressChangedEvent(
+      address.id,
+      new Date(),
+      correlationId,
+      {
+        ownerUserId: address.ownerUserId,
+      },
+    );
+  }
+}
