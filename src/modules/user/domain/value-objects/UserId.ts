72db5bc7b20962b1d5e2b25e94ec039b73441ce7
OlamidotunIY <olamidotun225@gmail.com>
2026-07-31 15:16:39 +0100

    feat(user): introduce user identity model

---
+++ src/modules/user/domain/value-objects/UserId.ts
@@ -0,0 +1,15 @@
+import { EntityId } from '@src/common';
+
+export class UserId extends EntityId {
+  constructor(value: string) {
+    super(value);
+  }
+
+  static create(): UserId {
+    return new UserId(crypto.randomUUID());
+  }
+
+  static fromString(value: string): UserId {
+    return new UserId(value);
+  }
+}
