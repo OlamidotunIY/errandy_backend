6124c5774a25c1bfa2e2b1ea8f5b9591b010d1a4
OlamidotunIY <olamidotun225@gmail.com>
2026-08-01 22:46:27 +0100

    fix(address): make IAddressRepository async and nullable

--- src/modules/address/domain/repository/IAddressRepository.ts
+++ src/modules/address/domain/repository/IAddressRepository.ts
@@ -2,7 +2,8 @@ import { Address, AddressId } from '@module/address';
 import { UserId } from '@module/user';
 
 export abstract class IAddressRepository {
-  abstract save(address: Address): void;
-  abstract findById(id: AddressId): Address;
-  abstract findByUserId(id: UserId): Address;
+  abstract save(address: Address): Promise<void>;
+  abstract findById(id: AddressId): Promise<Address | null>;
+  abstract findByUserId(id: UserId): Promise<Address[]>;
+  abstract delete(id: AddressId): Promise<void>;
 }
