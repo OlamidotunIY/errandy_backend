78bd55e057b8aefab01c532cbd5f9fc6d9cccf1e
OlamidotunIY <olamidotun225@gmail.com>
2026-08-03 16:36:53 +0100

    feat(app): import all active modules into root app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,15 @@ import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { GqlConfig } from '@src/config/graphql-ws';
 import { GraphqlExceptionFilter } from '@src/common/filters/graphql-exception.filter';
+import { PrismaModule } from './prisma/prisma.module';
 import { NotificationModule } from '@module/notification/notification.module';
+import { PartyModule } from '@module/party/party.module';
+import { AddressModule } from '@module/address/address.module';
+import { EscrowModule } from '@module/escrow/escrow.module';
+import { ServiceModule } from '@module/service/service.module';
+import { CategoryModule } from '@module/category/category.module';
+import { WalletModule } from '@module/wallet/wallet.module';
+import { ChatModule } from '@module/chat/chat.module';
 
 @Module({
   imports: [
@@ -13,7 +21,15 @@ import { NotificationModule } from '@module/notification/notification.module';
     }),
     GqlConfig,
     EventEmitterModule.forRoot(),
+    PrismaModule,
     NotificationModule,
+    PartyModule,
+    AddressModule,
+    EscrowModule,
+    ServiceModule,
+    CategoryModule,
+    WalletModule,
+    ChatModule,
   ],
   controllers: [],
   providers: [
