e66383c2218e1cb96804d174b8f0aec370ad7cc1
OlamidotunIY <olamidotun225@gmail.com>
2026-08-03 21:21:04 +0100

    feat(application): update request-application-acceptance command to handle payment authorization

--- src/modules/application/application/commands/request-application-acceptance/request-application-acceptance.payload.ts
+++ src/modules/application/application/commands/request-application-acceptance/request-application-acceptance.payload.ts
@@ -4,4 +4,5 @@ import { ApplicationId } from '@src/modules';
 export interface RequestApplicationAcceptancePayload {
   clientPartyId: PartyId;
   applicationId: ApplicationId;
+  paymentMethodId: string;
 }
